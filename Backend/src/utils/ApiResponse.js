68d171547d69a8bb419ef2472cf1363827fe778a
vishaljati <jativishal120@gmail.com>
2025-12-07 17:14:25 +0530

    Add api error and res utils

---
+++ Backend/src/utils/ApiResponse.js
@@ -0,0 +1,14 @@
+class ApiResponse {
+    constructor(
+        statuscode,
+        message = "",
+        data
+    ) {
+        this.statusCode = statuscode
+        this.data = data
+        this.message = message
+        this.success = statuscode < 400
+    }
+}
+
+export { ApiResponse }
