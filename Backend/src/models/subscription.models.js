9ab6acd75fab2b75c0df68c91d9c3475f64c55b5
vishaljati <jativishal120@gmail.com>
2025-12-11 19:35:25 +0530

    Add subscription model

--- Backend/src/models/subscription.models.js
+++ Backend/src/models/subscription.models.js
@@ -1,5 +1,34 @@
 import mongoose from "mongoose";
 
-const subscriptionSchema=new mongoose.Schema({},{})
+const subscriptionSchema=new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        unique:true
+    },
+    plan:{
+        enum:["free","pro","enterprise"],
+        default:"free"
+    },
+    status:{
+        enum:["active","cancelled"],
+        default:"active"
+    },
+    currentPeriodStart:{
+        type:Date,
+        required:true
+    },
+    currentPeriodEnd:{
+        type:Date,
+        required:true
+    },
+    stripeCustomerId:{
+        type:String,    
+    },
+    stripeSubscriptionId:{
+        type:String,
+    }
+
+},{timestamps:true})
 
 export const Subscription=mongoose.model("Subscription",subscriptionSchema)
